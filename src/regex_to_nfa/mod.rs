5d71f91593c696c22ba0c4d7f413d8bb8e6e15bc
Nick Vathis <nvathis@gmail.com>
2017-09-11 11:56:07 +0300

    add optional and range

--- src/regex_to_nfa/mod.rs
+++ src/regex_to_nfa/mod.rs
@@ -1,47 +1,45 @@
-use super::{StateId, NFANext, NFANextElem};
+use super::{StateId, SymbolId, NFANext, NFANextElem};
 
 mod parser;
 mod parser_utils;
 
 use self::parser_utils::Ast;
 
-fn regex_to_nfa_inner(ast: Box<Ast>, acc: &mut NFANext)
+fn new_node (acc: &mut NFANext, id: SymbolId, next: Vec<StateId>)
 {
-    let ast_deref = *ast;
-    match ast_deref {
-        Ast::Terminal(id) => {
-            let mut hmap: NFANextElem = NFANextElem::new();
+    let mut hmap: NFANextElem = NFANextElem::new();
+    hmap.insert(id, next);
+    acc.push(hmap);
+}
+
+fn new_dummy_node(acc: &mut NFANext) -> StateId
+{
+    let len = acc.len() as StateId;
+    new_node(acc, -1, vec![len + 1]);
+    len
+}
+
+fn regex_to_nfa_inner(ast: &Ast, acc: &mut NFANext)
+{
+    match ast {
+        &Ast::Terminal(id) => {
             let end = acc.len() as StateId;
-            hmap.insert(id, vec![end + 1]);
-            acc.push(hmap);
+            new_node(acc, id, vec![end + 1]);
         },
-        Ast::Cons(a1, a2) => {
+        &Ast::Cons(ref a1, ref a2) => {
             regex_to_nfa_inner(a1, acc);
             regex_to_nfa_inner(a2, acc);
         },
-        Ast::Star(a) => {
-            let mut hmap: NFANextElem = NFANextElem::new();
-            let start = acc.len() as StateId;
-            hmap.insert(-1, vec![start + 1]);
-            acc.push(hmap);
-            let mut hmap: NFANextElem = NFANextElem::new();
+        &Ast::Star(ref a) => {
+            let start = new_dummy_node(acc);
             regex_to_nfa_inner(a, acc);
             let end = acc.len() as StateId;
-            hmap.insert(-1, vec![start, end + 1]); // might return or continue
-            acc[start as usize].entry(-1).or_insert(Vec::new()).push(end + 1); // might skip
-            acc.push(hmap);
-
-            // New node to ensure all returns are from end, for backpatch
-            let mut hmap: NFANextElem = NFANextElem::new();
-            let new_node = acc.len() as StateId;
-            hmap.insert(-1, vec![new_node + 1]);
-            acc.push(hmap);
+            new_node(acc, -1, vec![start, end + 1]); // might return or continue
+            acc[start as usize].get_mut(&-1).unwrap().push(end + 1); // might skip
+            new_dummy_node(acc);
         }
-        Ast::Or(a1, a2) => {
-            let mut hmap: NFANextElem = NFANextElem::new();
-            let new_node = acc.len() as StateId;
-            hmap.insert(-1, vec![new_node + 1]);
-            acc.push(hmap);
+        &Ast::Or(ref a1, ref a2) => {
+            let dummy_node = new_dummy_node(acc);
             regex_to_nfa_inner(a1, acc);
             let a1_end = acc.len() as StateId - 1;
             let a2_start = acc.len() as StateId;
@@ -56,7 +54,27 @@ fn regex_to_nfa_inner(ast: Box<Ast>, acc: &mut NFANext)
                 }
             }
             // Make new_node skip to a2
-            acc[new_node as usize].entry(-1).or_insert(Vec::new()).push(a2_start);
+            acc[dummy_node as usize].get_mut(&-1).unwrap().push(a2_start);
+        },
+        &Ast::Optional(ref a) => {
+            let start = new_dummy_node(acc);
+            regex_to_nfa_inner(a, acc);
+            let end = acc.len() as StateId;
+            acc[start as usize].get_mut(&-1).unwrap().push(end + 1); // might skip
+            new_dummy_node(acc);
+        },
+        &Ast::Range(ref a, min, max) => {
+            new_dummy_node(acc);
+            for _ in 0..min {
+                regex_to_nfa_inner(a, acc);
+            }
+            for _ in min..max {
+                let start = new_dummy_node(acc);
+                regex_to_nfa_inner(a, acc);
+                let end = acc.len() as StateId;
+                acc[start as usize].get_mut(&-1).unwrap().push(end); // might skip
+            }
+            new_dummy_node(acc);
         }
     }
 }
@@ -68,7 +86,7 @@ pub fn regex_to_nfa(regex: &str) -> Result<NFANext, String>
         Err(x) => return Err(format!("{:?}", x))
     };
     let mut ret: NFANext = NFANext::new();
-    regex_to_nfa_inner(ast, &mut ret);
+    regex_to_nfa_inner(&ast, &mut ret);
     ret.push(NFANextElem::new());
     Ok(ret)
 }
--- src/regex_to_nfa/parser_utils.rs
+++ src/regex_to_nfa/parser_utils.rs
@@ -6,5 +6,7 @@ pub enum Ast
     Terminal(SymbolId),
     Cons(Box<Ast>, Box<Ast>),
     Star(Box<Ast>),
-    Or(Box<Ast>, Box<Ast>)
+    Or(Box<Ast>, Box<Ast>),
+    Optional(Box<Ast>),
+    Range(Box<Ast>, i32, i32)
 }
